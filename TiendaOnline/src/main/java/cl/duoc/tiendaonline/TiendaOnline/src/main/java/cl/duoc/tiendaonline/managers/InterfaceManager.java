d5d6b8db7919d4c569005f42435052968205d470
Totallypro <pablcar@gmail.com>
2025-08-18 23:09:21 -0400

    Add files via upload

---
+++ TiendaOnline/src/main/java/cl/duoc/tiendaonline/TiendaOnline/src/main/java/cl/duoc/tiendaonline/managers/InterfaceManager.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package cl.duoc.tiendaonline.managers;
+
+import cl.duoc.tiendaonline.managers.descuentos.DescuentoA;
+import cl.duoc.tiendaonline.managers.descuentos.DescuentoN;
+
+/**
+ * 
+ * @author Home
+ */
+public class InterfaceManager {
+    private final DescuentoA adidas = new DescuentoA(); 
+    private final DescuentoN nike = new DescuentoN();
+
+    public void mostrarDescuentos(int totalAdidas, int totalNike) { 
+        totalAdidas = adidas.run(totalAdidas);  
+        totalNike = nike.run(totalNike);      
+        
+        DiscountManager dm = DiscountManager.getInstancia();
+        
+        System.out.println("");
+        System.out.println("=== Descuentos aplicados ===");
+        System.out.println("Descuento A (Adidas): " + dm.get("A"));
+        System.out.println("Descuento N (Nike): " + dm.get("N"));
+        System.out.println("============================");
+    }
+}
+ 
